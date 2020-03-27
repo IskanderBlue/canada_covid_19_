37b62ff052dc7a1535928d4c4fc94ff4498937af
Robert Lee <iskander.lee@gmail.com>
2020-03-27 02:41:22 -0400

    Derandomize colours; comment code.

--- app.R
+++ app.R
@@ -1,4 +1,9 @@
 library(tidyverse)
+
+# Loading data
+deaths <- read.csv("time_series_covid19_deaths_global.csv")
+
+# Setting slider orientation
 ori <- "horizontal"
 if (ori == "horizontal") {
   hei <- "30px"
@@ -9,7 +14,8 @@ if (ori == "horizontal") {
 } else {
   stop("Invalid ori.")
 }
-deaths <- read.csv("time_series_covid19_deaths_global.csv")
+
+# Group by country, sum deaths, drop duplicates, transpose, drop China, factorize dates.
 deaths_by_country <- deaths %>% 
   dplyr::group_by(Country.Region) %>% 
   dplyr::mutate_at(dplyr::vars(dplyr::starts_with("X")), sum) %>% 
@@ -23,6 +29,12 @@ deaths_by_country <- deaths %>%
   dplyr::mutate(`Date` = gsub("X", "", `Date`)) %>%
   dplyr::mutate(`Date` = factor(`Date`, levels = `Date`))
 
+#' Takes list of countries' death data, joins them together by days since X deaths.  
+#' 
+#' Takes list of countries' death data, joins them together by days column.  
+#' If you run into errors here, there may be bad data; had an error when 
+#' Philippines' 3.18.20 deaths were weirdly high and then dropped back down on the 19th.
+#' 
 #' @param x integer, which element of lst, which column of dfm to tack
 #' @param lst list, each element contains list of 2 elements: 1) data for a country 2) cutoff position for that country
 #' @param dfm dataframe with structure to tack lst data together by
@@ -37,17 +49,39 @@ tack_days_since <- function(x, lst, dfm, back, offset = 0) {
   names(tacked) <- c("days", nm)
   return(tacked)
 }
+
+#' Drops all elements from a vector that are cumulatively smaller than geq.
+#' 
+#' Drops all elements from a vector that are cumulatively smaller than geq.
+#' Returns a list with 
+#' 1) the presumably shortened vector and 
+#' 2) the position (in the input vector) of the first element in the returned vector.  
+#' @param x vector of numerics
+#' @param geq numeric value, cutoff
 dropper <- function(x, geq) {
   vec <- x[cumsum(x) >= geq]
   frst <- min(which(cumsum(x) >= geq))
   return(list(vec, frst))
 }
+
+#' Finds the position of the last non-NA element of a vector.
+#' 
+#' Finds the position of the last non-NA element of a vector.
+#' If the desired maximum prediction period is longer than the vector, returns entire vector.  
+#' Otherwise, returns the positions of the last `prediction_period` elements from the vector.
+#' @param vec vector, presumably with NA elements at the end.
+#' @param pred_per integer, maximium prediction period.
 find_position <- function(vec, pred_per) {
   last_non_na <- max(which(!is.na(vec)))
   max(last_non_na - pred_per, 1):last_non_na
 }
 
-
+#' Predicts subsequent values of an exponentially changing vector.  
+#' 
+#' For a given `coef` and `reduction` of that coefficient, returns `vec` with 
+#' an additional `iterations` elements that have increased exponentially by 
+#' `coef` with `reduction` added slowly as a lognormal distribution.  
+#' See onset to death, https://www.mdpi.com/2077-0383/9/2/538/htm
 #' @param vec vector of integers, predicted values
 #' @param iterations, integer, number of iterations to extend vec by
 predict_for_na <- function(vec, iterations, coef, reduction) {
@@ -79,40 +113,41 @@ predict_for_na <- function(vec, iterations, coef, reduction) {
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application that shows how slowly changes in 
+# Covid-19's growth rate affect deaths. 
 ui <- shinydashboard::dashboardPage(
   # Application title
   title = "Covid-19 Projection",
   shinydashboard::dashboardHeader(
-    title = shiny::textOutput("days_since")),
+    title = shiny::textOutput("days_since")), # Adjusts 'days since' according to min deaths inputted.
   shinydashboard::dashboardSidebar(
-    shiny::uiOutput("country_selector"), shiny::br(),
-    shinyWidgets::noUiSliderInput(
+    shiny::uiOutput("country_selector"), shiny::br(), # Selects countries to include
+    shinyWidgets::noUiSliderInput( # Slider to set countermeasure effectiveness
       inputId = "r", label = "Countermeasure effectiveness:", min = 0, max = 0.99, 
       value = 0.5, step = 0.01, orientation = ori, 
       format = shinyWidgets::wNumbFormat(decimals = 2), 
       color = "#2980b9", inline = TRUE,
       height = hei, width = wid), shiny::br(), 
-    shinyWidgets::noUiSliderInput(
+    shinyWidgets::noUiSliderInput( # Slider to set prediction period
       inputId = "pred_per", label = "Max. prediction period:", min = 3, max = 20, 
       value = 10, step = 1, orientation = ori, 
       format = shinyWidgets::wNumbFormat(decimals = 0), 
       color = "#2980b9", inline = TRUE,
       height = hei, width = wid), shiny::br(), 
     
-    shinyWidgets::noUiSliderInput(
+    shinyWidgets::noUiSliderInput( # Slider to set minimum cumulative deaths at d+0.
       inputId = "mind", label = "Minimum deaths:", min = 10, max = 25, 
       value = 10, step = 1, orientation = ori, 
       format = shinyWidgets::wNumbFormat(decimals = 0), 
       color = "#c0392b", inline = TRUE,
       height = hei, width = wid), shiny::br(), 
-    shinyWidgets::noUiSliderInput(
-      inputId = "days", label = "Days shown:", min = 1, max = 60, 
+    shinyWidgets::noUiSliderInput( # Slider to adjust how many days included in graph.
+      inputId = "days", label = "Days shown:", min = 3, max = 60, 
       value = 40, step = 1, orientation = ori, 
       format = shinyWidgets::wNumbFormat(decimals = 0), 
       color = "#27ae60", inline = TRUE,
       height = hei, width = wid), 
-    shinyWidgets::noUiSliderInput(
+    shinyWidgets::noUiSliderInput( # Slider to adjust how many days before minimum deaths are reached to include.
       inputId = "back", label = "d-x shown:", min = 1, max = 10, 
       value = 3, step = 1, orientation = ori, 
       format = shinyWidgets::wNumbFormat(decimals = 0), 
@@ -121,109 +156,122 @@ ui <- shinydashboard::dashboardPage(
     shiny::numericInput("plt_hei", "Plot Height:", value = 650)
   ),
   shinydashboard::dashboardBody(
-    plotly::plotlyOutput("days_since_min")
+    plotly::plotlyOutput("days_since_min") # Show plot.
   )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required
 server <- function(input, output, session) {
-  
+  # Adjusts 'days since' according to min deaths inputted.
   output$days_since <- shiny::renderText({paste0("Days since ", input$mind, " Deaths")})
-  
+  # Returns a list of dataframes with 
+  # 1) daily deaths joined by days since cumulative minimum deaths reached, and 
+  # 2) the dates of those deaths.
   valid_countries <- shiny::reactive({
-    countries_with_deaths <- deaths_by_country[
+    countries_with_deaths <- deaths_by_country[ # Drop countries with insufficient deaths.
       c(FALSE, sapply(deaths_by_country[2:length(deaths_by_country)], 
                       function(x) max(x, na.rm = T) > input$mind))]
-    deaths_by_day <- as.data.frame(diff(as.matrix(countries_with_deaths))) 
-    death_dates <- apply(deaths_by_day, 2, function(x) {as.character(deaths_by_country$Date[-1])})
-    dropped <- apply(deaths_by_day, 2, dropper, input$mind)
-    tacked <- lapply(1:length(dropped), tack_days_since, dropped, deaths_by_day, input$back)
-    tacked_dates <- lapply(1:length(dropped), tack_days_since, dropped, death_dates, input$back)
+    deaths_by_day <- as.data.frame(diff(as.matrix(countries_with_deaths))) # Subtract each row from next; get daily change in deaths.
+    death_dates <- apply(deaths_by_day, 2, function(x) {as.character(deaths_by_country$Date[-1])}) # Replace all death numbers with their dates (for later use)
+    dropped <- apply(deaths_by_day, 2, dropper, input$mind) # Get list (won't fit in data.frame) of vectors of deaths after the cumulative minimum is reached (along with the position of the first element)
+    tacked <- lapply(1:length(dropped), tack_days_since, dropped, deaths_by_day, input$back) # Tack days since cumulative minimum reached onto daily deaths for later joining.
+    tacked_dates <- lapply(1:length(dropped), tack_days_since, dropped, death_dates, input$back) # Tack days since cumulative minimum reached onto daily deaths' dates for later joining.
+    # Merge the data (and then the dates) together by days since cumulative minimum was reached.
     merged <-       merge(tacked[[1]], tacked[[2]], by = "days", all = TRUE)
-    merged_dates <- merge(tacked_dates[[1]], tacked_dates[[2]], by = "days", all = TRUE)
+    merged_dates <- merge(tacked_dates[[1]], tacked_dates[[2]], by = "days", all = TRUE) 
     for (i in 3:length(tacked)) {
       merged <- merge(merged, tacked[[i]], by = "days", all = TRUE)
       merged_dates <- merge(merged_dates, tacked_dates[[i]], by = "days", all = TRUE)
     }
+    # Truncate (or extend) rows according to how many days we want to show in the graph.
     merged <- merged[1:input$days, ]
     merged_dates <- merged_dates[1:input$days, ]
     return(list(merged, merged_dates))
   })
 
+  # Build plotly plot 
   plt <- shiny::reactive({
     countries <- input[["countries_selected"]]
     plt <- plotly::plot_ly(type = 'scatter', mode = 'lines+markers',
                            height = input$plt_hei) 
-
+    color_iter <- c(1, 2)
     for (i in countries) {
       plt_df <- valid_countries()[[1]] %>% 
-        dplyr::rename(y=i) %>% 
-        dplyr::mutate(periods = (input$back * -1):(nrow(.)-(input$back + 1)) ) %>%
-        dplyr::mutate(z = valid_countries()[[2]][, i]) %>% 
-        dplyr::mutate(days = c(paste0("d", (input$back * -1):(-1)), 
+        dplyr::rename(y=i) %>% # Rename the country to y for ease of use
+        dplyr::mutate(periods = (input$back * -1):(nrow(.)-(input$back + 1)) ) %>% # get periods eg. c(-3, -1, -2, 0, 1, 2, 3, 4)
+        dplyr::mutate(z = valid_countries()[[2]][, i]) %>% # Add row with dates for country y
+        dplyr::mutate(days = c(paste0("d", (input$back * -1):(-1)), # add dates for all rows (otherwise no x values beyond last current date)
                                paste0("d+", 0:(nrow(.)-(input$back + 1)))))
       
-      fit_pos <- find_position(plt_df$y, input$pred_per) 
+      fit_pos <- find_position(plt_df$y, input$pred_per) # Get positions of deaths to fit model
       periods <- plt_df$periods[fit_pos] # select only periods desired to fit model
-      mdl <- lm(log(plt_df$y[fit_pos]+0.01) ~ periods)
+      mdl <- lm(log(plt_df$y[fit_pos]+0.01) ~ periods) # fit model
       itrs <- plt_df$periods[length(plt_df$periods)] - periods[length(periods)] # Number of times to iterate through predict_not_na
-      print(plt_df$periods[1:fit_pos[length(fit_pos)]])
-      predicted <- exp(predict(mdl, list(periods=plt_df$periods[1:fit_pos[length(fit_pos)]])))
-      print(predicted)
-      predicted <- predict_for_na(predicted, itrs, mdl$coefficients[[2]], input$r)
+      predicted <- exp(predict(mdl, list(periods=plt_df$periods[1:fit_pos[length(fit_pos)]]))) # "Predict" up to present with model
+      predicted <- predict_for_na(predicted, itrs, mdl$coefficients[[2]], input$r) # predict based on model and reduction in R
       plt_df$preds <- predicted
-      print(predicted)
-      trace_col <- paste0("rgb(", paste0(sample(255, 3), collapse=", "), ")")
 
       plt <- plt %>%
+        # Add prediction lines
         plotly::add_trace(data = plt_df, x = ~days, y = ~preds, 
-                          name = i, 
-                          line = list(color = trace_col), showlegend = FALSE,
+                          name = i,
+                          # line = list(color = trace_col), 
+                          showlegend = FALSE,
                           mode = 'lines', 
                           hoverinfo = "text+name", 
                           hovertemplate = "%{y:.2f} predicted for") %>%
+        # Add death data points
         plotly::add_trace(data = plt_df, x = ~days, y = ~y, 
-                          name = i, 
+                          name = i,
                           mode = 'markers', 
-                          marker = list(color = trace_col), 
+                          # marker = list(color = trace_col), 
                           hoverinfo = "text+name",
                           text = ~paste0(z, ", ", y, " deaths in"))
     }
-    
-    plt <- plt %>% 
-      plotly::layout(yaxis = list(title = "Deaths")
-                                  # , 
-                                  # paper_bgcolor = "rgb(0, 0, 0)",
-                                  # plot_bgcolor = "rgb(34, 45, 50)"
+    plt <- plt %>% # Fix layout: y-axis, colours.
+      plotly::layout(yaxis = list(title = "Daily Deaths"),
+                     colorway = c("#FFFFFF", RColorBrewer::brewer.pal(
+                       n = (length(countries) * 2), name = "Paired"))
+                     # paper_bgcolor = "rgb(0, 0, 0)", # Background behind/surrounding plot
+                     # plot_bgcolor = "rgb(34, 45, 50)" # Plot background
                      )
     return(plt)
   })
   output$days_since_min <- plotly::renderPlotly({plt()})
   
+  # UI for selecting countries.
   output$country_selector <- shiny::renderUI({
-    shiny::selectInput(
+    shiny::selectizeInput(
       "countries_selected", label = "Select countries:", 
       choices = shiny::isolate(names(valid_countries()[[1]])[-1]), 
-      selected = c("Canada", "US", "Italy", "Korea, South"), 
-      multiple = TRUE)
+      selected = c("Canada"), #, "US", "Italy", "Korea, South"), 
+      multiple = TRUE, 
+      options = list(maxItems = 6))
   })
   
+  # When valid countries changes, change country selection options.
   shiny::observeEvent(valid_countries()[[1]], {
     selected_countries <- input$countries_selected
-    shiny::updateSelectInput(session, "countries_selected", 
+    shiny::updateSelectizeInput(session, "countries_selected", 
                             choices = names(valid_countries()[[1]])[-1], 
                             selected = selected_countries)
   }, ignoreInit = TRUE)
   
+  # Ensure days before min deaths reached never crowd out d+0.
   shiny::observeEvent(input$days, {
     backs <- input$back
     if (input$days <= 10) {
       shinyWidgets::updateNoUiSliderInput(session, "back", value = backs, 
-                                          range = c(0, input$days))
+                                          range = c(1, input$days-1))
+    } else {
+      shinyWidgets::updateNoUiSliderInput(session, "back", value = backs, 
+                                          range = c(1, 10))
     }
   }, ignoreInit = TRUE)
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+# Update shinyapps.io
 # rsconnect::deployApp()
